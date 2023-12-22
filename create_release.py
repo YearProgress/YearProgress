2bf86f4e735e2ba4b0ec486f644d573e5963fa80
ZincCat <zincchloride@outlook.com>
2023-12-22 01:58:47 -0500

    added bar, changed to EST

--- create_release.py
+++ create_release.py
@@ -8,6 +8,12 @@ def percent_of_year_passed():
     end_of_year = datetime(now.year + 1, 1, 1)
     return ((now - start_of_year).total_seconds() / (end_of_year - start_of_year).total_seconds()) * 100
 
+def create_progress_bar(percentage, bar_length=40):
+    # Using a block for filled part and a dot for unfilled
+    filled_length = int(round(bar_length * percentage / 100))
+    bar = '█' * filled_length + '░' * (bar_length - filled_length)
+    return bar
+
 def create_github_release(token, repo, tag_name, name, body):
     url = f"https://api.github.com/repos/{repo}/releases"
     headers = {
@@ -30,7 +36,9 @@ if __name__ == "__main__":
     repo = "YearProgress/YearProgress"
     tag_name = f"Progress-{datetime.now().strftime('%Y%m%d')}"
     name = f"{datetime.now().strftime('%Y-%m-%d')}"
-    body = f"Year Progress: {progress:.2f}%"
+    progress_percentage = percent_of_year_passed()
+    progress_bar = create_progress_bar(progress_percentage)
+    body = f"# Year Progress\n{progress_bar} {progress_percentage:.2f}%"
 
     response = create_github_release(token, repo, tag_name, name, body)
     print(response.json())
--- progress.py
+++ progress.py
@@ -6,5 +6,15 @@ def percent_of_year_passed():
     end_of_year = datetime(now.year + 1, 1, 1)
     return ((now - start_of_year).total_seconds() / (end_of_year - start_of_year).total_seconds()) * 100
 
+def create_progress_bar(percentage, bar_length=40):
+    # Using a block for filled part and a dot for unfilled
+    filled_length = int(round(bar_length * percentage / 100))
+    bar = '█' * filled_length + '░' * (bar_length - filled_length)
+    return bar
+
 if __name__ == "__main__":
-    print(f"Year Progress: {percent_of_year_passed():.2f}%")
+    progress_percentage = percent_of_year_passed()
+    progress_bar = create_progress_bar(progress_percentage)
+    readme_content = f"# Year Progress\n{progress_bar} {progress_percentage:.2f}%"
+    with open('README.md', 'w') as file:
+        file.write(readme_content)
